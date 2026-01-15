c51660850b8f34aafe3b609e0b9bd1465fa4a1ef
Kankana <kankanaghosh129@gmail.com>
2026-01-15 16:41:12 +0530

    learned composition

--- OOPs.py
+++ OOPs.py
@@ -287,36 +287,64 @@
 #     print(animal.alive)
 
 
+
 #Aggregation
 
-class Library:
-    def __init__(self,name):
-        self.name = name
-        self.books = []
+# class Library:
+#     def __init__(self,name):
+#         self.name = name
+#         self.books = []
         
-    def add_book(self,book):
-        self.books.append(book)  
+#     def add_book(self,book):
+#         self.books.append(book)  
         
-    def list_books(self): 
-        return [f"{book.title} by {book.author}" for book in self.books]    
+#     def list_books(self): 
+#         return [f"{book.title} by {book.author}" for book in self.books]    
 
-class Book:
-    def __init__(self,title,author):
-        self.title = title
-        self.author = author
+# class Book:
+#     def __init__(self,title,author):
+#         self.title = title
+#         self.author = author
         
-library = Library("New york times")
-book1 = Book("mili ghosh","kankana")
-book2 = Book("ABCD","eGFn")
-book3 = Book("hello","hey") 
+# library = Library("New york times")
+# book1 = Book("mili ghosh","kankana")
+# book2 = Book("ABCD","eGFn")
+# book3 = Book("hello","hey") 
 
-library.add_book(book1)  
-library.add_book(book2)   
-library.add_book(book3)  
+# library.add_book(book1)  
+# library.add_book(book2)   
+# library.add_book(book3)  
 
-print(library.name)   
+# print(library.name)   
 
-for book in library.list_books():
-    print(book)  
+# for book in library.list_books():
+#     print(book)  
+
+
+#composition
+
+class Engine:
+    def __init__(self,horse_power):
+        self.horse_power = horse_power
+        
+class Wheel:
+    def __init__(self,size):
+        self.size = size
+        
+class Car:
+    def __init__(self, make,model,horse_power,wheel_size):
+        self.make = make
+        self.model = model  
+        self.engine = Engine(horse_power)
+        self.wheels = [Wheel(wheel_size) for wheel in range(4)]
+        
+    def display_car(self):
+        return f"{self.make} {self.model} {self.engine.horse_power}(hp) {self.wheels[0].size}in"   
+        
+car = Car(make = "ford",model = "Mustang", horse_power = 500,wheel_size = 18) 
+print(car.display_car())       
+              
+                
+        
     
                        
